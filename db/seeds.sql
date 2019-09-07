8a7de062815e5974a5bc5de0b0a09e8c1e6d3223
PCHIII <treyhelmer@gmail.com>
2019-09-07 16:31:18 -0400

    npm init

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon Cheese Burger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Deep Fried Bacon Cheese Burger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Double Bacon Cheese Burger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Triple Bacon Cheese Burger', true);
